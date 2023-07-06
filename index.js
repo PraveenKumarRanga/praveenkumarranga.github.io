dae29739b7e308046d14b8b2c9c8193f7019b43f
jai bhole <praveenkr.ranga@gmail.com>
2023-07-06 22:30:41 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var textElement = document.querySelector('.fade-in-fade-out');
+    var texts = ['Praveen Kumar Ranga',  'Java Backend Developer'];
+    var currentTextIndex = 0;
+  
+    function changeText() {
+      textElement.textContent = texts[currentTextIndex];
+      currentTextIndex = (currentTextIndex + 1) % texts.length;
+    }
+    changeText();
+    setInterval(changeText, 3000);
+  });
