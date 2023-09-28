6ffbbc92d203b151ec00ef9e58fd1a3975639763
jai bhole <praveenkr.ranga@gmail.com>
2023-09-28 13:49:17 +0530

    trail 2 portfolio

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ window.addEventListener('DOMContentLoaded', function() {
     changeText();
     setInterval(changeText, 3000);
   });
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-list");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => 
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }))
